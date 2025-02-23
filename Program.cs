05a33d7c8d812c98eb3a2d7af8cf125a60527f21
kosta-atanasov <kostaatanasovpc99@gmail.com>
2025-02-23 22:19:14 +0200

    add file

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете година");
+        int num = int.Parse(Console.ReadLine());
+
+            if (num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
+           
+            {
+                Console.WriteLine("Годината е високосна");
+
+
+            }
+            else 
+            {
+                Console.WriteLine("Годината не е високосна");
+
+
+            }
+        }
+    }
+}
